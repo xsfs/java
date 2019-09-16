0a7ea34ea3f8255b7865e03749d6c455e4e7d224
zhangzhuohui <609976350@qq.com>
2019-09-16 15:45:46 +0800

    test

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        double l = 99.45;
+        System.out.printf("%+f\n",l);
+        System.out.printf("%-3d\n",10);
+        System.out.printf("%,f\n",99999.22);
+        System.out.printf("%f和%<3.2f",l);
+    }
+}
